a599f5c026c7c2e6459dea347611d50507292fe8
Sven Latham (Noggin) <sven@noggin.bi>
2020-02-09 22:17:50 +0000

    Removed rsync requirement for openvpn

--- openvpn-client/apply.bash
+++ openvpn-client/apply.bash
@@ -7,7 +7,8 @@ if [ ! -f "/home/pi/openvpn/vpn.conf" ]; then
     echo "Please first upload the config file for this device to /home/pi/openvpn/vpn.conf"
     exit 1
 fi
-sudo apt-get install -y rsync openvpn
+sudo apt-get install -y openvpn
+
 sudo cp /home/pi/openvpn/vpn.conf /etc/openvpn/client/vpn.conf
 sudo systemctl enable openvpn-client@vpn.service
 sudo systemctl preset openvpn-client@vpn.service
